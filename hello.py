cf1d209ca38a1c90807705c5d39a6821340e1dcc
harunra <muhammadharunalrasyid_2428250068@mhs.mdp.ac.id>
2025-11-25 14:12:21 +0700

    1

--- hello.py
+++ hello.py
@@ -1,25 +1,49 @@
-# import Flask module
-from flask import Flask, render_template
 
-# create Flask app instance (atur folder template ke 'views')
-app = Flask(__name__, template_folder='views')
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+
+class ContactForm(FlaskForm):
+    name = StringField('Nama', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    submit = SubmitField('Kirim')
+    born = StringField('Tanggal Lahir')
+    school = StringField('Asal Sekolah')
+    cellphone = StringField('No. Handphone')
 
 
-# define route for the root URL
-@app.route('/')
-def hello_world():
-    return render_template('main.html')
 
-# define route untuk about
-@app.route('/about')
-def about():
-    return render_template('about.html')
-   
-   # define route untuk contact
-@app.route('/contact')
+
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    form = ContactForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        # Proses data
+        return redirect(url_for('thank_you'))
+    return render_template('contact.html', form=form)
 
-# run the app
+@app.route('/pmb', methods=['GET', 'POST'])
+def pmb():
+    form = PmbForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        born = form.born.data
+        school = form.school.data
+        cellphone = form.cellphone.data
+        # Proses data
+        return redirect(url_for('thank_you'))
+    return render_template('contact.html', form=form)
+
+@app.route('/thank-you')
+def thank_you():
+    return "Terima kasih telah mengirimkan form!"
+
 if __name__ == '__main__':
     app.run(debug=True)
